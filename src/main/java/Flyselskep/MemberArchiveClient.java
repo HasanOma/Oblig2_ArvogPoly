ebdddc8b6aeaf128c8fcde508ddad9140c93d1d3
Hasan Rehman Omarzae <hasanro@stud.ntnu.no>
2021-02-09 22:13:43 +0100

    Finnished

--- src/main/java/Flyselskep/MemberArchiveClient.java
+++ src/main/java/Flyselskep/MemberArchiveClient.java
@@ -6,6 +6,8 @@ package Flyselskep;
  * and open the template in the editor.
  */
 
+import java.time.LocalDate;
+
 /**
  * A simple client program to test the functionality of the
  * MemberArchive and underlaying classes.
@@ -23,6 +25,8 @@ public class MemberArchiveClient {
             System.out.println(member.toString());
         }
 
+
+
         System.out.println("\nAdd some bonuspoints to all of the members..\n");
 
         System.out.println("Member 1 gets 10.000");
@@ -39,6 +43,21 @@ public class MemberArchiveClient {
 
         System.out.println("Member 5 gets 10.000");
         memberArchive.registerPoints(5, 10000);
+
+        System.out.println("Now lets see the register:\n");
+        for (BonusMember member: memberArchive.allMembers()){
+            System.out.println(member.toString());
+        }
+
+
+//        BonusMember newMember = new BonusMember(6, LocalDate.now(), 45000, "Olsen, ben", "ben@olsen.biz");
+//        memberArchive.addMember(newMember);
+//        BonusMember newMember2 = new BonusMember(7, LocalDate.now(), 70000, "Olsen, ken", "ken@olsen.biz");
+//        memberArchive.addMember(newMember2);
+//        BonusMember newMember3 = new BonusMember(8, LocalDate.now(), 80000, "Olsen, jon", "jon@olsen.biz");
+//        memberArchive.addMember(newMember3);
+//        BonusMember newMember4 = new BonusMember(9, LocalDate.now(), 9000, "Olsen, sven", "sven@olsen.biz");
+//        memberArchive.addMember(newMember4); //Adding members trough addMember method
 
         System.out.println("Now lets see the register:\n");
         for (BonusMember member: memberArchive.allMembers()){
--- src/test/java/Flyselskep/MemberArchiveTest.java
+++ src/test/java/Flyselskep/MemberArchiveTest.java
@@ -13,3 +13,4 @@ class MemberArchiveTest {
     void registerPoints() {
     }
 }
+
